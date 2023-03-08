e68a9248f0f00097d6dba8c985883090a9098a3f
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2023-03-08 23:21:54 +0530

    Enabling communication within microservices

--- HotelService/HotelService/src/main/java/com/Icwd/hotel/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Icwd.hotel.repositories;
-
-import com.Icwd.hotel.entities.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
--- User-Service/src/main/java/com/Icwd/user/service/services/impl/UserServiceImpl.java
+++ User-Service/src/main/java/com/Icwd/user/service/services/impl/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.Icwd.user.service.services.impl;
 
+import com.Icwd.user.service.entities.Rating;
 import com.Icwd.user.service.exceptions.ResourceNotFoundException;
 import com.Icwd.user.service.repositories.UserRepository;
 import com.Icwd.user.service.entities.User;
 import com.Icwd.user.service.services.UserService;
+import com.netflix.discovery.converters.Auto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +21,10 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -32,6 +42,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(("User with given id is not found on the server!! : "+userId)));
+        //get user from db with the help of user repo
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException(("User with given id is not found on the server!! : " + userId)));
+        //fetch rating of the above user from RATING SERVICE
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("{}",ratingsOfUser);
+        user.setRatings(ratingsOfUser);
+        return user;
     }
 }
