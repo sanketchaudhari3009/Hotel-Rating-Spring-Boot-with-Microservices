f19f8b50230ab19a48e11a8006499309bf0adf04
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2023-03-11 22:41:25 +0530

    Adding OTKA Oauth to Hotel and Rating Service

--- HotelService/src/main/java/com/Icwd/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/Icwd/hotel/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.Icwd.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class HotelController {
     private HotelService hotelService;
 
     //create
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
@@ -24,12 +26,14 @@ public class HotelController {
 
 
     //get single
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> createHotel(@PathVariable String hotelId) {
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.get(hotelId));
     }
 
     //get all
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAll() {
         return ResponseEntity.ok(hotelService.getAll());
--- RatingService/src/main/java/com/Icwd/rating/controllers/RatingController.java
+++ RatingService/src/main/java/com/Icwd/rating/controllers/RatingController.java
@@ -5,6 +5,7 @@ import com.Icwd.rating.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +18,27 @@ public class RatingController {
     private RatingService ratingService;
 
     //create rating
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Rating> create(@RequestBody Rating rating) {
         return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
     }
 
     //get all
+
     @GetMapping
     public ResponseEntity<List<Rating>> getRatings() {
         return ResponseEntity.ok(ratingService.getRatings());
     }
 
-    //get all
+    //get all user
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> getRatingsByUserId(@PathVariable String userId) {
         return ResponseEntity.ok(ratingService.getRatingByUserId(userId));
     }
 
-    //get all
+    //get all hotels
     @GetMapping("/hotels/{hotelId}")
     public ResponseEntity<List<Rating>> getRatingsByHotelId(@PathVariable String hotelId) {
         return ResponseEntity.ok(ratingService.getRatingByHotelId(hotelId));
